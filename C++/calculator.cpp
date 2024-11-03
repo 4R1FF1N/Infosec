d728e8ffd815b91520b16855a532a9b0d613e102
GR1FF1N <125984037+4R1FF1N@users.noreply.github.com>
2024-11-03 19:02:47 +0000

    Create calculator.cpp

---
+++ C++/calculator.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int first_number, second_number, sum;
+
+cout << "Enter two integers bitch: ";
+cin >> first_number >> second_number;
+
+sum = first_number + second_number;
+
+cout << first_number << " + " << second_number << " = " << sum;
+
+    return 0;
+}
